c96d32cfdfcbbebc3e7bff13a8c81648d08d3687
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-16 15:16:00 -0300

    feat: (anima3): Soldier Movement.

--- js/js.js
+++ js/js.js
@@ -5,6 +5,6 @@ function start() {
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1'class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 }
