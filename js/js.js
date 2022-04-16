fc38eaae8a389347cb4b847009c154ed0c07cff1
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-16 14:43:43 -0300

    feat: (function-start): Creating function Start.

---
+++ js/js.js
@@ -0,0 +1,10 @@
+// Start function start()
+function start() { 
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+}
