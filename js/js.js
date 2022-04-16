b1abebb6564edec0338afd66764854884695647f
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-16 15:02:29 -0300

    feat: (functions): Add anima1 and anima2.

--- js/js.js
+++ js/js.js
@@ -2,8 +2,8 @@
 function start() { 
 	$("#inicio").hide();
 	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1'class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo'></div>");
 
