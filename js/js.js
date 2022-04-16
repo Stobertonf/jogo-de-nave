78345f9bb78f6f98b7bec861a8a4b8e8c92bbc6d
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-16 15:48:51 -0300

    feat: (js): Creating helicopter motion.

--- js/js.js
+++ js/js.js
@@ -57,13 +57,24 @@ function start() {
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top",topo-10);
+
+            if (topo<=0) {
+		
+                $("#jogador").css("top",topo+10);
+            }
+            
         
         }
         
         if (jogo.pressionou[TECLA.S]) {
             
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+            $("#jogador").css("top",topo+10);
+            
+            if (topo>=434) {	
+                $("#jogador").css("top",topo-10);
+                    
+            }
         }
         
         if (jogo.pressionou[TECLA.D]) {
